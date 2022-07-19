f44154a36b8096705a4724852835ad4c7d69e23a
Nahiyan09 <87262306+Nahiyan09@users.noreply.github.com>
2022-07-19 19:33:38 +0100

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "PALLET TOWN";
+const accessCode2 = "FOX";
+const accessCode3 = "BLACK NICHIRIN";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("L A L E T P O N W T", 100,50)
+    fill("lightblue")
+    text("Hint: A town that the main character of pokemon comes from?", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("O F X", 700,150)
+    fill("lightblue")
+    text("Hint: Animal inside of naruto", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("L C K A B I N H C I N I R", 100,250)
+    fill("lightblue")
+    text("Hint: Tanjiro's first sword", 100,270)
+
+}
